882725b530e3ffa405fd209d78c470f38461fd76
Bartosz Staręga <bartosz.starega@gmail.com>
2020-06-30 00:39:16 +0200

    Updates

---
+++ precache-manifest.f3dded3db6a8ab147e1f00122b70b18a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad128201dfb23808689d11baeef2a06",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "af4420753a6a03cadcc2",
+    "url": "/react-app-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c7ca18d2162d1b428891",
+    "url": "/react-app-test/static/js/2.1fc63d76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-test/static/js/2.1fc63d76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4420753a6a03cadcc2",
+    "url": "/react-app-test/static/js/main.cc449911.chunk.js"
+  },
+  {
+    "revision": "c26a2022d756e2b268a1",
+    "url": "/react-app-test/static/js/runtime-main.2e777044.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
